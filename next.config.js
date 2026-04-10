7efbeb13c1a12e18523ce00addd1c107c48ab6fb
mylesb345-code <mylesb345@gmail.com>
2026-04-10 11:02:52 -0500

    Create next.config.js for Next.js configuration

---
+++ next.config.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @type {import('next').NextConfig}
+ **/
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ['example.com'], // Add your image domains here
+  },
+};
+
+module.exports = nextConfig;
